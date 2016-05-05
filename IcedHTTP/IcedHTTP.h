320eb5214c399295f317b3349b7e46d242907275
Alf Watt <alf@istumbler.net>
2016-05-05 11:29:18 -0700

    Initial Commit

---
+++ IcedHTTP/IcedHTTP.h
@@ -0,0 +1,19 @@
+//
+//  IcedHTTP.h
+//  IcedHTTP
+//
+//  Created by Alf Watt on 5/5/16.
+//  Copyright © 2016 iStumbler Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IcedHTTP.
+FOUNDATION_EXPORT double IcedHTTPVersionNumber;
+
+//! Project version string for IcedHTTP.
+FOUNDATION_EXPORT const unsigned char IcedHTTPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IcedHTTP/PublicHeader.h>
+
+
